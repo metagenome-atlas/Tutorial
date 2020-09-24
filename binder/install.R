3a2bb2cc7d84c02cbfabb198d284fb596c94788c
kiesers <silas.kieser@gmail.com>
2020-09-24 22:07:49 +0200

    use one conda env for r and python

--- binder/install.R
+++
@@ -1,5 +0,0 @@
-### install regular packages
-
-
-install.packages(c("rmarkdown", "ape", "broom", "dplyr", "ggplot2",
- "pheatmap", "phytools", "plotly", "purrr", "readr", "stringr","tibble", "tidyr", "vegan"))
