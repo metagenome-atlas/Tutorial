bfa26472ba988790ccfc0237b4e5e041a6f97288
kiesers <silas.kieser@gmail.com>
2020-09-24 21:58:17 +0200

    Ad binder for Rmarkdown

---
+++ binder/install.R
@@ -0,0 +1,5 @@
+### install regular packages
+
+
+install.packages(c("rmarkdown", "ape", "broom", "dplyr", "ggplot2",
+ "pheatmap", "phytools", "plotly", "purrr", "readr", "stringr","tibble", "tidyr", "vegan"))
