f03fc2b9a429d560697495a94d86f1cd4132a118
silask <silas.kieser@gmail.com>
2020-11-30 10:40:40 +0100

    Update Differential_abundance, remove text, add p value correction

--- Python/utils/mag_scripts.py
+++ Python/utils/mag_scripts.py
@@ -90,3 +90,48 @@ def get_kegg_names(json_file):
     Names= pd.Series(Names,name='Name')
     
     return Names
+
+def correct_pvalues_for_multiple_testing(pvalues, correction_type = "Benjamini-Hochberg"):
+    """
+    correction_type: one of "Bonferroni", "Bonferroni-Holm", "Benjamini-Hochberg"
+    consistent with R - print correct_pvalues_for_multiple_testing([0.0, 0.01, 0.029, 0.03, 0.031, 0.05, 0.069, 0.07, 0.071, 0.09, 0.1])
+    """
+    from numpy import array, empty
+    pvalues = array(pvalues)
+
+    #sort p vlaues and prepare unsort index
+    sort_index = np.argsort(pvalues)
+    pvalues= pvalues[sort_index]
+    unsort_index = np.argsort(sort_index)
+
+    n = pvalues.shape[0]
+    new_pvalues = empty(n)
+    n= float(n)
+
+
+    if correction_type == "Bonferroni":
+        new_pvalues = n * pvalues
+    elif correction_type == "Bonferroni-Holm":
+        values = [ (pvalue, i) for i, pvalue in enumerate(pvalues) ]
+        values.sort()
+        for rank, vals in enumerate(values):
+            pvalue, i = vals
+            new_pvalues[i] = (n-rank) * pvalue
+    elif correction_type == "Benjamini-Hochberg":
+        values = [ (pvalue, i) for i, pvalue in enumerate(pvalues) ]
+        values.sort()
+        values.reverse()
+        new_values = []
+        for i, vals in enumerate(values):
+            rank = n - i
+            pvalue, index = vals
+            new_values.append((n/rank) * pvalue)
+        for i in range(0, int(n)-1):
+            if new_values[i] < new_values[i+1]:
+                new_values[i+1] = new_values[i]
+        for i, vals in enumerate(values):
+            pvalue, index = vals
+            new_pvalues[index] = new_values[i]
+
+
+    return new_pvalues[unsort_index]
